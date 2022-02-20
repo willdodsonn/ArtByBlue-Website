4e73dd80efd3018810adfcdd97be0be0eb74cab0
willdodsonn <sodlax38@gmail.com>
2022-02-20 19:32:23 +0000

    blues webiste start

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaintBrush } from "@fortawesome/free-solid-svg-icons";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const StartPage = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="container">
+					<h1 className="navbar-brand">
+						<a
+							class="navbar-brand"
+							href="https://www.instagram.com/art.by.blue/?hl=en">
+							<FontAwesomeIcon icon={faPaintBrush} /> Art By Blue{" "}
+						</a>
+					</h1>
+				</div>
+			</nav>
+			<div className="jumbotron jumbotron-fluid text-center">
+				<img
+					className="jumbotron-img-top"
+					src="https://images.squarespace-cdn.com/content/v1/604d215e22e44a51184c40bd/1622988262899-Q0DQ7RT4QMZ8VY177JFN/Blue+Dunsworth+reduced+-+Studios+of+Cocoa+Beach.jpg"
+					alt="Jumbotron image cap"
+				/>
+				<div className="container">
+					<h1 className="display-4">
+						Full Collection of Blue Dunsworth
+					</h1>
+					<p className="lead">
+						Prices may vary, contact Blue for more information
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default StartPage;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,75 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import StartPage from "./component/home.jsx";
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import Carousel from "react-bootstrap/Carousel";
+import Card from "./component/Card.jsx";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+const RandomPage = () => {
+	return (
+		<div>
+			<div>
+				<StartPage />
+			</div>
+			<div>
+				<Carousel>
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src="https://i1.sndcdn.com/artworks-000201256683-45rbuy-t500x500.jpg"
+							alt="First slide"
+						/>
+						<Carousel.Caption>
+							<h3>First slide label</h3>
+							<p>
+								Nulla vitae elit libero, a pharetra augue mollis
+								interdum.
+							</p>
+						</Carousel.Caption>
+					</Carousel.Item>
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src="https://i1.sndcdn.com/avatars-000182957187-axj0eh-t500x500.jpg"
+							alt="Second slide"
+						/>
+
+						<Carousel.Caption>
+							<h3>Second slide label</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit.
+							</p>
+						</Carousel.Caption>
+					</Carousel.Item>
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOOXWxNrlFNtB37C05yxFXm2qTqYEWbK374rWPMukWlNsZXrAG_MHZtQJap1xB0LpSXvQ&usqp=CAU"
+							alt="Third slide"
+						/>
+
+						<Carousel.Caption>
+							<h3>Third slide label</h3>
+							<p>
+								Praesent commodo cursus magna, vel scelerisque
+								nisl consectetur.
+							</p>
+						</Carousel.Caption>
+					</Carousel.Item>
+				</Carousel>
+			</div>
+			<div>
+				<Card />
+			</div>
+		</div>
+	);
+};
+export default RandomPage;
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<RandomPage />, document.querySelector("#app"));
